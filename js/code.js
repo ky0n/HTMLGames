b909affd7be74bb7a32de655202cc77f9f26f761
Daniel Schmelzer <daniel.schmelzer@stud.htwk-leipzig.de>
2017-08-10 14:12:07 +0200

    bugfixes

--- js/code.js
+++ js/code.js
@@ -1,6 +1,6 @@
 //Globale Var
 var svgNS = "http://www.w3.org/2000/svg";
-var colors = ["rgb(158, 158, 158)", "rgb(255, 87, 34)", "rgb(233, 30, 99)", "rgb(156, 39, 176)", "rgb(33, 150, 243)", "rgb(255, 235, 59)", "rgb(139, 195, 74)"]; //Spiel Farben
+var colors = ["rgb(255, 87, 34)", "rgb(233, 30, 99)", "rgb(156, 39, 176)", "rgb(33, 150, 243)", "rgb(255, 235, 59)", "rgb(139, 195, 74)"]; //Spiel Farben
 var solutionColors = []; // Farbe die gelöst werden soll
 var currentRow = 0; //Aktuelle Zeile
 
@@ -16,10 +16,18 @@ window.onload = function () {
     }
     var elements = document.getElementsByClassName("mdl-button");
     for (i = 0; i < elements.length; i++) {
-        elements[i].addEventListener("click", changeColor);
         elements[i].style.backgroundColor = "rgb(158, 158, 158)";
     }
     generateSet();
+    clickListenerForCurrentRow(currentRow)
+}
+
+function clickListenerForCurrentRow(currentRow) {
+    var elements = document.getElementsByClassName("mdl-button");
+    for (i = currentRow * 4; i < currentRow * 4 + 4; i++) {
+        elements[i].addEventListener("click", changeColor);
+
+    }
 
 }
 
@@ -52,8 +60,20 @@ function changeColor(e) {
 }
 
 function generateSet() {
-    for (i = 0; i < 4; i++) {
-        var r = Math.floor((Math.random() * 6) + 0);
+    // for (i = 0; i < 4; i++) {
+    //     var r = Math.floor((Math.random() * 6) + 0);
+    //     solutionColors.push(colors[r]);
+    //     var btn = document.getElementById("rb" + (i + 1));
+    //     btn.style.backgroundColor = colors[r];
+    // }
+    for (i = 0; i < 2; i++) {
+        var r = Math.floor(2);
+        solutionColors.push(colors[r]);
+        var btn = document.getElementById("rb" + (i + 1));
+        btn.style.backgroundColor = colors[r];
+    }
+    for (i = 2; i < 4; i++) {
+        var r = Math.floor(1);
         solutionColors.push(colors[r]);
         var btn = document.getElementById("rb" + (i + 1));
         btn.style.backgroundColor = colors[r];
@@ -64,21 +84,36 @@ function logic() {
     var duploesung = JSON.parse(JSON.stringify(solutionColors));
 
     var korrektePosition = 0;
-    var korrekteFarbe =0;
+    var korrekteFarbe = 0;
     var buttonrow = document.getElementsByClassName("buttonrow")[currentRow];
+
+    var eingabefarben = [];
     for (var i = 0; i < 4; i++) {
         var button = buttonrow.getElementsByClassName('b' + (i + 1))[0];
-        if (button.style.backgroundColor === solutionColors[i]) {
+        eingabefarben.push(button.style.backgroundColor);
+    }
+
+    for (var i = 0; i < 4; i++) {
+        if (eingabefarben[i] === solutionColors[i]) {
             korrektePosition++;
-        }
-        else {
-            var pos = duploesung.indexOf(button.style.backgroundColor);
-            if (pos > -1){
-                korrekteFarbe++;
-                duploesung[pos] = "";
-            }
+            eingabefarben[i] = "a";
+            duploesung[i] = "";
         }
     }
+    for (var i = 0; i < 4; i++) {
+        var pos = duploesung.indexOf(eingabefarben[i]);
+        if (pos > -1) {
+            korrekteFarbe++;
+            eingabefarben[i] = "a";
+            duploesung[i] = "";
+        }
+    }
+
+    if (korrektePosition === 4) {
+        alert("Gewonnen");
+    }
+    currentRow++;
+    clickListenerForCurrentRow(currentRow);
     //alert("pos: " + korrektePosition + " farb: " + korrekteFarbe);
     colorSVG(buttonrow, korrektePosition, korrekteFarbe);
 }
@@ -86,13 +121,13 @@ function logic() {
 function colorSVG(buttonrow, korrektePosition, korrekteFarbe) {
     for (var i = 0; i < korrekteFarbe; i++) {
         var svg = buttonrow.getElementsByTagName("svg")[0];
-        var svgcircle = svg.getElementById("mycircle"+i);
+        var svgcircle = svg.getElementById("mycircle" + i);
         svgcircle.setAttributeNS(null, "fill", "white");
 
     }
-    for (i = korrekteFarbe; i <korrekteFarbe + korrektePosition; i++ ){
+    for (i = korrekteFarbe; i < korrekteFarbe + korrektePosition; i++) {
         var svg = buttonrow.getElementsByTagName("svg")[0];
-        var svgcircle = svg.getElementById("mycircle"+i);
+        var svgcircle = svg.getElementById("mycircle" + i);
         svgcircle.setAttributeNS(null, "fill", "black");
     }
 
