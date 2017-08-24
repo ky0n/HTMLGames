9b2d307103acc9b616202c2a281a49010a9769c5
Romy Spangenberg <romy.spangenberg@stud.htwk-leipzig.de>
2017-08-24 21:27:08 +0200

    Vue-Objekt für alle JS-Attribute nutzen

--- games/flood/flood.js
+++ games/flood/flood.js
@@ -1,15 +1,20 @@
 /**
  * Created by romy on 22.08.2017.
  */
-//var colors = ["green", "red", "blue"]; //TODO find good colors
-var colors = ["rgb(255, 87, 34)", "rgb(156, 39, 176)", "rgb(33, 150, 243)", "rgb(255, 235, 59)", "rgb(139, 195, 74)", "cyan"];
-var firstColor;
-
 
 new Vue({
     el: '#flood',
 
     data: {
+        colors: [ //TODO find good colors
+            {color: "rgb(255, 87, 34)"},
+            {color: "rgb(156, 39, 176)"},
+            {color: "rgb(33, 150, 243)"},
+            {color: "rgb(255, 235, 59)"},
+            {color: "rgb(139, 195, 74)"},
+            {color: "cyan"}
+        ],
+        firstColor: "",
         rows: [
             // wird bei Initialisierung (created Methode) belegt
         ],
@@ -17,7 +22,7 @@ new Vue({
 
     methods: {
         changeColor: function(item){
-            if (item.color === firstColor) {
+            if (item.color === this.firstColor) {
                 return;
             }
 
@@ -28,20 +33,16 @@ new Vue({
 
     created(){
         let tempRows = [];
-
         for (let i = 0; i < 9; i++) {
             let tempC = [];
             for (let j = 0; j < 9; j++) {
-                //TODO better solution for random numbers
-                let r = Math.floor((Math.random() * 5));
-                let color = colors[r];
-
+                let r = Math.floor(Math.random() * (this.colors.length));
+                let color = this.colors[r].color;
                 tempC[j] = {color: color};
             }
             tempRows[i] = {columns: tempC};
         }
-
-        firstColor = tempRows[0].columns[0].color;
+        this.firstColor = tempRows[0].columns[0].color;
         this.rows = tempRows;
     }
 });
