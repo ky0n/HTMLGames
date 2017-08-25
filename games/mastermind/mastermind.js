a3c2d3653a85f0572080513d3d953a4cafa8806a
Daniel Schmelzer <daniel.schmelzer@stud.htwk-leipzig.de>
2017-08-25 13:36:50 +0200

    #13 mastermind.js, mastermind.css, mastermind.html in eigenem ordner erstellt und indexnew-test.html in index.html umbenannt

--- games/mastermind/mastermind.js
+++ games/mastermind/mastermind.js
@@ -1,7 +1,7 @@
 //Globale Var
 var svgNS = "http://www.w3.org/2000/svg";
 var colors = ["rgb(255, 87, 34)", "rgb(156, 39, 176)", "rgb(33, 150, 243)", "rgb(255, 235, 59)", "rgb(139, 195, 74)", "cyan"]; //Spiel Farben
-var solutionColors = []; // Farbe die gelöst werden soll
+//var solutionColors = []; // Farbe die gelöst werden soll
 var currentRow = 0; //Aktuelle Zeile
 var gray = "rgb(158, 158, 158)";
 var white = "white";
@@ -14,19 +14,6 @@ var cyan = "cyan";
 window.onload = function () {
     createCircle();
 
-    for (var i = 0; i < 9; i++) {
-        var buttonRow = getButtonRow(0);
-
-        var clone = buttonRow.cloneNode(true);
-
-        document.getElementById("buttons").appendChild(clone);
-    }
-    var elements = document.getElementsByClassName("btn-circle");
-    for (i = 0; i < elements.length; i++) {
-         elements[i].style.backgroundColor = gray;
-    }
-    generateSet();
-    clickListenerForCurrentRow(currentRow);
 };
 
 function removeSVGClickListener() {
@@ -100,20 +87,8 @@ function createCircle() {
     }
 }
 
-function changeColor(e) {
-    var btn = e.target;
-    var color = btn.style.backgroundColor;
-    btn.style.backgroundColor = colors[(colors.indexOf(color) + 1) % colors.length];
-}
 
-function generateSet() {
-    for (var i = 0; i < 4; i++) {
-        var r = Math.floor((Math.random() * 5));
-        solutionColors.push(colors[r]);
-        var btn = document.getElementById("rb" + (i + 1));
-        btn.style.backgroundColor = colors[r];
-    }
-}
+
 
 function logic() {
     var duploesung = JSON.parse(JSON.stringify(solutionColors));
@@ -160,3 +135,86 @@ function colorSVG(korrektePosition, korrekteFarbe) {
     colorCircle(korrekteFarbe,  korrekteFarbe+korrektePosition, black);
     colorCircle(korrektePosition+korrekteFarbe, 4, gray);
 }
+
+let buttons = new Vue({
+    el: '#buttons',
+
+    data: {
+        colors: [ "rgb(255, 87, 34)", "rgb(156, 39, 176)", "rgb(33, 150, 243)", "rgb(255, 235, 59)", "rgb(139, 195, 74)", "cyan"],
+        rows: [
+            // wird bei Initialisierung (created Methode) belegt
+        ],
+    },
+
+    methods: {
+        changeColor: function(item){
+            var color = item.color;
+            item.color = this.colors[(this.colors.indexOf(color) + 1) % this.colors.length];
+        }
+    },
+
+    created(){
+        let tempRows = [];
+        for (let i = 0; i < 9; i++) {
+            let tempC = [];
+            for (let j = 0; j < 4; j++) {
+                tempC[j] = {color: gray};
+            }
+            tempRows[i] = {columns: tempC};
+        }
+        this.firstColor = tempRows[0].columns[0].color;
+        this.rows = tempRows;
+    }
+});
+
+new Vue({
+    el: '#result',
+
+    data: {
+        solutionColors: [],
+        row: [
+            // wird bei Initialisierung (created Methode) belegt
+        ],
+    },
+
+    methods: {
+    },
+
+    created(){
+
+        let tempC = [];
+        for (let i = 0; i < 4; i++) {
+            let r = Math.floor((Math.random() * 5));
+            this.solutionColors.push(colors[r]);
+            tempC[i] = {color: colors[r]};
+        }
+        this.row = {columns: tempC};
+    }
+});
+
+
+new Vue({
+    el: '#mySVG',
+
+
+
+    createCircle() {
+    var zahl = 0;
+    for (var i = 12; i < 38; i += 25) {
+        for (var j = 12; j < 38; j += 25) {
+
+            var myCircle = document.createElementNS(svgNS, "circle"); //to create a circle. for rectangle use "rectangle"
+            myCircle.setAttributeNS("", "id", "mycircle" + zahl);
+            myCircle.setAttributeNS("", "cx", ""+i);
+            myCircle.setAttributeNS("", "cy", ""+j);
+            myCircle.setAttributeNS("", "r", "10");
+            myCircle.setAttributeNS("", "fill", gray);
+            myCircle.setAttributeNS("", "stroke", "none");
+
+            //document.getElementById("mySVG").appendChild(myCircle);
+            zahl++;
+        }
+    }
+}
+
+});
