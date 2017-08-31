0b913c83836a277b0189cb860e63d558750f25bf
Arne Laake <arne_vincent.laake@stud.htwk-leipzig.de>
2017-08-31 22:42:30 +0200

    Link zu neuem Video

--- games/untangle/untangle.js
+++ games/untangle/untangle.js
@@ -1,66 +1,58 @@
-var can = document.getElementById("can");
-
-can.width = window.innerWidth;
-can.height = window.innerHeight;
-
-c = can.getContext("2d");
-
-window.addEventListener('mousemove', function(event){
-    mouse.x = event.x;
-    mouse.y = event.y;
-})
-
-//------responsiveCanvas---------------------------
-$(document).ready( function(){
-    //Get the canvas & context 
-    var rc = $('#can');
-    var ct = rc.get(0).getContext('2d');
-    var container = $(rc).parent();
-
-    //Run function when browser resizes
-    $(window).resize( respondCanvas );
-
-    function respondCanvas(){
-        rc.attr('width', Math.max(document.documentElement.clientWidth, window.innerWidth || 0) ); //max width
-        rc.attr('height', Math.max(document.documentElement.clientHeight, window.innerHeight || 0) ); //max height
-
-        //Call a function to redraw other content (texts, images etc)
-    }
-
-    //Initial call
-    respondCanvas();
-}); 
-
-
-//----data--------------------------------------------
-var mouse = {
-    x: undefined,
-    y: undefined
-}
-
-
-
-
-
-
-//---main-------------------------------------------------
-
-console.log("HI");
-animate = function(){
-    requestAnimationFrame(animate);
-    c.clearRect(0,0,innerWidth,innerHeight);
-
-}
-
 //-----VUE--------------------------------------------------------------------------------
 
 new Vue({
-  el: '#can',
+    el: '#wrapper',
     data: {
-        
+        mouse:{
+            x: 0,
+            y: 0,
+        },
+        canvas: undefined,
+        c: undefined,
+        thumbs:[],
     },
     methods: {
+        init: function() {
+
+        },
+        update: function(event){
+            this.mouse.x = event.clientX;
+            this.mouse.y = event.clientY;
+        },
+        generate: function () {
+            let canvas = document.getElementById("game");
+            canvas.width = window.innerWidth;
+            canvas.height = window.innerHeight;
+            this.canvas = canvas;
+            let c = this.canvas.getContext("2d");
+            this.c = c;
+            for(let i= 0; i< 7; i++){
+                posx = Math.floor(Math.random()* 500)
+                posy = Math.floor(Math.random()* 500);
+                this.c.beginPath();
+                this.c.fillStyle = "black";
+                this.c.arc(posx,posy,20,0,2*Math.PI,false);
+                this.c.fill();
+            }
+
+        }
+    },
+    /*created(){
 
     }
-    
+    watch: {
+
+    },
+    mounted: {
+
+    },
+    beforeUpdate:{
+
+    },
+    updated:{
+
+    }*/
+    //wie initialize
+
 })
+
