a96eded3434baf09e5bd8ef9fa012478863988dc
Romy Spangenberg <romy.spangenberg@stud.htwk-leipzig.de>
2017-09-03 16:47:52 +0200

    "Neustart" und "Neues Spiel" implementiert

--- games/flood/flood.js
+++ games/flood/flood.js
@@ -14,46 +14,67 @@ new Vue({
             {color: 'cyan'}
         ],
         squareSize: 9,
+        oldGame: [],
         rows: [
             // wird bei Initialisierung belegt
         ],
         moves: 0,
+        maxMoves: 20,
+        coloredFields: 0,
+        endOfGame: false,
     },
 
     methods: {
         initialize: function () {
+            this.moves = 0;
+            this.endOfGame = false;
             let tempRows = [];
+            this.oldGame = [];
             for (let i = 0; i < this.squareSize; i++) {
                 let tempColumns = [];
+                let tempColumnsOld = [];
                 for (let j = 0; j < this.squareSize; j++) {
                     let r = Math.floor(Math.random() * (this.colors.length));
                     let color = this.colors[r].color;
                     tempColumns[j] = {color: color, borderStyleClass: 'none', index: -1};
+                    tempColumnsOld[j] = {color: color};
                 }
                 tempRows[i] = {columns: tempColumns};
+                this.oldGame[i] = {columns: tempColumnsOld};
             }
             this.rows = tempRows;
 
             let color = this.rows[0].columns[0].color;
             this.floodFill(0, 0, color, color);
+
+            for (let i = 0; i < this.squareSize; i++) {
+                for (let j = 0; j < this.squareSize; j++) {
+                    this.changeBorderStyle(i, j);
+                }
+            }
         },
 
-        changeColor: function(item){
+        changeColor: function(newColor){
             let oldColor = this.rows[0].columns[0].color;
-            let newColor = item.color;
 
-            if (oldColor === newColor) {
+            if (oldColor === newColor || this.moves >= this.maxMoves || this.endOfGame) {
                 return;
             }
 
             this.moves++;
+
+            this.coloredFields = 0;
             this.floodFill(0, 0, oldColor, newColor);
             for (let i = 0; i < this.squareSize; i++) {
                 for (let j = 0; j < this.squareSize; j++) {
                     if (this.rows[i].columns[j].index != -1 && this.rows[i].columns[j].borderStyleClass != 'four') {
-                        this.changeBorder(i, j);
+                        this.changeBorderStyle(i, j);
                     }
                 }
+            }
+
+            if (this.coloredFields === this.squareSize * this.squareSize) {
+                this.endOfGame = true;
             }
         },
 
@@ -69,6 +90,7 @@ new Vue({
 
             if (this.rows[x].columns[y].color === oldColor && this.rows[x].columns[y].index != this.moves) {
                 this.rows[x].columns[y].color = newColor;
+                this.coloredFields++;
                 this.floodFill(x+1, y, oldColor, newColor);
                 this.floodFill(x, y+1, oldColor, newColor);
                 this.floodFill(x-1, y, oldColor, newColor);
@@ -83,6 +105,7 @@ new Vue({
 
             if (this.rows[x].columns[y].color === color && this.rows[x].columns[y].index === -1) {
                 this.rows[x].columns[y].index = this.moves;
+                this.coloredFields++;
                 this.borderFlood(x+1, y, color);
                 this.borderFlood(x, y+1, color);
                 this.borderFlood(x-1, y, color);
@@ -90,7 +113,7 @@ new Vue({
             }
         },
         
-        changeBorder: function (x, y) {
+        changeBorderStyle: function (x, y) {
             let color = this.rows[x].columns[y].color;
             let neighbours = this.checkNeighbours(x, y, color);
 
@@ -283,15 +306,32 @@ new Vue({
             }
 
             return {left: l, right: r, top: t, bottom: b, cornerLT: cLT, cornerLB: cLB, cornerRT: cRT, cornerRB: cRB};
-        }
+        },
+
+        restart: function () {
+            for (let i = 0; i < this.squareSize; i++) {
+                for (let j = 0; j < this.squareSize; j++) {
+                    this.rows[i].columns[j].color = this.oldGame[i].columns[j].color;
+                    this.rows[i].columns[j].borderStyleClass = 'none';
+                    this.rows[i].columns[j].index = -1;
+                }
+            }
+
+            this.moves = 0;
+            this.coloredFields = 0;
+            this.endOfGame = false;
+
+            let color = this.rows[0].columns[0].color;
+            this.floodFill(0, 0, color, color);
+            for (let i = 0; i < this.squareSize; i++) {
+                for (let j = 0; j < this.squareSize; j++) {
+                    this.changeBorderStyle(i, j);
+                }
+            }
+        },
     },
 
     created(){
         this.initialize();
-        for (let i = 0; i < this.squareSize; i++) {
-            for (let j = 0; j < this.squareSize; j++) {
-                this.changeBorder(i, j);
-            }
-        }
     }
 });
