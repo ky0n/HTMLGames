d92817c7b6dc0939e0859000cf027acb2be4a208
Arne Laake <arne_vincent.laake@stud.htwk-leipzig.de>
2017-10-04 12:29:04 +0200

    [#56] Bäume gepflanzt

--- games/tents/tents.js
+++ games/tents/tents.js
@@ -17,7 +17,36 @@ new Vue({
     methods:{
         initialize: function () {
             this.spawnTents();
+            this.spawnTrees();
             this.initializeFields();
+        },
+
+        initializeFields: function () {
+            let tempRows = [];
+            for (let i = 0; i < this.numOfRows; i++) {
+                let tempColumns = [];
+                for (let j = 0; j < this.numOfCols; j++) {
+                    let fieldIndex = i * this.numOfRows + j;
+                    let field = {};
+                    field.isTent = this.tents.includes(fieldIndex);
+                    field.isTree = this.trees.includes(fieldIndex);
+                    field.disabled = false;
+                    field.clicked = false;
+                    field.row = i;
+                    field.column = j;
+                    if(field.isTent){
+                        field.image = this.images.tent;
+                    }else if(field.isTree){
+                        field.image = this.images.tree;
+                    }else{
+                        field.image = this.images.empty;
+                    }
+                    tempColumns[j] = field;
+                }
+                tempRows[i] = {columns: tempColumns};
+                console.log(tempRows[i]);
+            }
+            this.rows = tempRows;
         },
 
         getRandomInt: function (min, max) {
@@ -32,14 +61,15 @@ new Vue({
                 let fieldFound = false;
                 while (!fieldFound) {
                     let k = Math.floor(Math.random() * (this.numOfRows * this.numOfCols));
-                    if (!this.tents.includes(k) && !this.findNeighbors(k)) {
+                    if (!this.tents.includes(k) && !this.foundNeighbors(k)) {
                         this.tents.push(k);
                         fieldFound = true;
                     }
                 }
             }
+            console.log(this.tents);
         },
-        findNeighbors: function (fieldNum) {
+        foundNeighbors: function (fieldNum) {
             let neighborFound = false;
             let neighbors = [];
             //Eckennummern
@@ -137,7 +167,8 @@ new Vue({
                     fieldNum + this.numOfRows +1
                 ];
             }
-            for(let i=0; i<neighbors.length; i++){
+            //return neighbors;
+            for(let i = 0; i < neighbors.length; i++){
                 if(this.tents.includes(neighbors[i])){
                     neighborFound = true;
                     break;
@@ -146,12 +177,16 @@ new Vue({
             return neighborFound;
         },
 
+        //---------------------------------------------------------------------------
+
         spawnTrees: function () {
             for (let i = 0; i < this.numOfTrees; i++) {
                 let fieldFound = false;
                 while (!fieldFound) {
-                    let k = Math.floor(Math.random() * (this.numOfRows * this.numOfCols));
-                    if (!this.trees.includes(k)) {
+                    let tent = this.tents[i];
+                    let neighbors = this.fieldsNextToTent(tent);
+                    let k = neighbors[Math.floor(Math.random() * (neighbors.length))];
+                    if (!this.trees.includes(k) && !this.tents.includes(k)) {
                         this.trees.push(k);
                         fieldFound = true;
                     }
@@ -159,28 +194,87 @@ new Vue({
             }
         },
 
-        initializeFields: function () {
-            let tempRows = [];
-            for (let i = 0; i < this.numOfRows; i++) {
-                let tempColumns = [];
-                for (let j = 0; j < this.numOfCols; j++) {
-                    let fieldIndex = i * this.numOfRows + j;
-                    let field = {};
-                    field.isTent = this.tents.includes(fieldIndex);
-                    field.disabled = false;
-                    field.clicked = false;
-                    field.row = i;
-                    field.column = j;
-                    if(field.isTent){
-                        field.image = this.images.tent;
-                    }else{
-                        field.image = this.images.empty;
-                    }
-                    tempColumns[j] = field;
-                }
-                tempRows[i] = {columns: tempColumns};
+        fieldsNextToTent: function (tent) {
+            let neighbors = [];
+            //Eckennummern
+            let leftUpperCorner = 0;
+            let rightUpperCorner = this.numOfRows - 1;
+            let leftLowerCorner = this.numOfCols*(this.numOfRows - 1);
+            let rightLowerCorner = this.numOfCols*this.numOfRows - 1;
+            //linker Rand
+            let lcol = [];
+            for(let i = 1; i < this.numOfRows-1; i++){
+                lcol.push(i*this.numOfRows);
             }
-            this.rows = tempRows;
+            //rechter Rand
+            let rcol = [];
+            for(let i = 2; i < this.numOfRows; i++){
+                rcol.push(i*this.numOfRows - 1);
+            }
+            //oberer Rand
+            let upperRow = [];
+            for(let i = 1; i < this.numOfRows; i++){
+                upperRow.push(i);
+            }
+            //unterer Rand
+            let lowerRow = [];
+            for(let i = leftLowerCorner + 1; i < rightLowerCorner; i++){
+                lowerRow.push(i);
+            }
+            //testen, ob der zu prüfende Index am Rand ist, um die richtigen Nachbarn zu bestimmen
+            if(tent === leftUpperCorner){
+                neighbors = [
+                    tent + 1,
+                    tent + this.numOfRows,
+                ];
+            }else if(tent === leftLowerCorner){
+                neighbors = [
+                    tent - this.numOfRows,
+                    tent + 1
+                ];
+            }else if(tent === (rightUpperCorner)){
+                neighbors = [
+                    tent + this.numOfRows,
+                    tent - 1
+                ];
+            }else if(tent === rightLowerCorner){
+                neighbors = [
+                    tent - this.numOfRows,
+                    tent - 1
+                ];
+            }else if(upperRow.includes(tent)){
+                neighbors = [
+                    tent -1,
+                    tent + 1,
+                    tent + this.numOfRows,
+                ];
+            }else if(lcol.includes(tent)){
+                neighbors = [
+                    tent - this.numOfRows,
+                    tent + 1,
+                    tent + this.numOfRows,
+                ];
+            }else if(rcol.includes(tent)){
+                neighbors = [
+                    tent - this.numOfRows,
+                    tent -1,
+                    tent + this.numOfRows,
+                ];
+            }else if(lowerRow.includes(tent)){
+                neighbors = [
+                    tent - this.numOfRows,
+                    tent -1,
+                    tent + 1,
+                ];
+            }else{
+                neighbors = [
+                    tent - this.numOfRows,
+                    tent -1,
+                    tent + 1,
+                    tent + this.numOfRows,
+                ];
+            }
+            return neighbors;
         },
 
         /*initializeFields: function () {
@@ -208,7 +302,7 @@ new Vue({
         },*/
 
         setTent: function (field) {
-            if(field.image === this.images.tree){
+            if(field.isTree){
 
             }else if(field.image === this.images.tent){
                 field.image = this.images.gras;
@@ -220,7 +314,7 @@ new Vue({
         },
 
         setGras: function (field) {
-            if(field.image === this.images.tree){
+            if(field.isTree){
 
             }else if(field.image === this.images.gras){
                 field.image = this.images.empty;
